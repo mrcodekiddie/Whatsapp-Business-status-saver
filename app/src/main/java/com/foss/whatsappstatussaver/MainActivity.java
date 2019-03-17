6cb26a8547f014ca555d10068065222e95bf12ae
Muthukumaraswami <vmuthu@tuta.io>
2019-03-17 19:48:44 +0530

    loaded images and videos 😋

--- app/src/main/java/com/foss/whatsappstatussaver/MainActivity.java
+++ app/src/main/java/com/foss/whatsappstatussaver/MainActivity.java
@@ -3,67 +3,94 @@ package com.foss.whatsappstatussaver;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    List<String> filePaths;
+    RecyclerView recyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if(!isFileReadPermissiongranted())
+        filePaths = new ArrayList<String>();
+        if (!isFileReadPermissiongranted())
         {
             showPermissionRequestAlertmessage();
-        }
-        else
+            readFiles();
+            proceedNext();
+        } else
         {
-            String path = Environment.getExternalStorageDirectory().toString()+"/WhatsApp Business/Media/.Statuses";
-            Log.d("Files", "Path: " + path);
-            File directory = new File(path);
-            File[] files = directory.listFiles();
-            Log.d("Files", "Size: "+ files.length);
-            for (int i = 0; i < files.length; i++)
-            {
-                Log.d("Files", "FileName:" + files[i].getName());
-                
-            }
+            readFiles();
+            proceedNext();
         }
-
 
 
     }
+
+    private void proceedNext()
+    {
+        recyclerView=findViewById(R.id.mRecyclerView);
+        StatusViewAdapter adapter=new StatusViewAdapter(filePaths,this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void readFiles()
+    {
+        String path = Environment.getExternalStorageDirectory().toString() + "/WhatsApp Business/Media/.Statuses";
+        Log.d("Files", "Path: " + path);
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        Log.d("Files", "Size: " + files.length);
+        for (int i = 0; i < files.length; i++)
+        {
+            Log.d("Files", "FileName:" + files[i].getName());
+            String filePath=files[i].getAbsolutePath();
+            Log.d("Filesx",filePath);
+            Log.d("FilesY",filePath.substring(filePath.length()-4));
+            filePaths.add(filePath);
+        }
+    }
+
     private void showPermissionRequestAlertmessage()
     {
-        AlertDialog.Builder alertDialog= new AlertDialog.Builder(this)
-                .setTitle("allow me please")
-                .setMessage("Allow me to read and write files inorder to save status")
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        showPermissionRequestDialog();
-                    }
-                });
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this).setTitle("allow me please").setMessage("Allow me to read and write files inorder to save status").setPositiveButton("ok", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                showPermissionRequestDialog();
+            }
+        });
         alertDialog.show();
     }
+
     private boolean isFileReadPermissiongranted()
     {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
         {
             return false;
-        }
-        else
+        } else
         {
             return true;
         }
@@ -71,25 +98,24 @@ public class MainActivity extends AppCompatActivity {
 
     private void showPermissionRequestDialog()
     {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                2);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
 
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode)
         {
-            case  2:
-                if((grantResults.length>0)&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            case 2:
+                if ((grantResults.length > 0) && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
                     Toast.makeText(this, "Hurray, I got permission, Thanks", Toast.LENGTH_SHORT).show();
-                }
-                else
+                } else
                 {
                     Toast.makeText(this, "Common.., I won't eat you.. Just allow me to write something to your files", Toast.LENGTH_SHORT).show();
+                    showPermissionRequestDialog();
                 }
         }
     }
---
+++ app/src/main/java/com/foss/whatsappstatussaver/StatusViewAdapter.java
@@ -0,0 +1,77 @@
+package com.foss.whatsappstatussaver;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class StatusViewAdapter extends RecyclerView.Adapter<StatusViewAdapter.StatusViewHolder>
+{
+    private List<String> filesPath;
+    private Context context;
+
+    public StatusViewAdapter(List<String> filesPath, Context context)
+    {
+        this.filesPath = filesPath;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public StatusViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+    {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_layout, viewGroup, false);
+        return new StatusViewAdapter.StatusViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StatusViewHolder statusViewHolder, int i)
+    {
+        String filePath = filesPath.get(i);
+
+        if (filePath.substring(filePath.length() - 3).equals("mp4"))
+        {
+            Bitmap thumb = ThumbnailUtils.createVideoThumbnail(filePath, MediaStore.Images.Thumbnails.MINI_KIND);
+            statusViewHolder.statusImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            statusViewHolder.statusImageView.setImageBitmap(thumb);
+
+        }
+
+        else
+        {
+            Bitmap bitmap = BitmapFactory.decodeFile(filesPath.get(i));
+            statusViewHolder.statusImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            statusViewHolder.statusImageView.setImageBitmap(bitmap);
+
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return filesPath.size();
+    }
+
+    public class StatusViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView statusImageView;
+
+        public StatusViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            statusImageView = itemView.findViewById(R.id.iv_status_image);
+        }
+    }
+}
